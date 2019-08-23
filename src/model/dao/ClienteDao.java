fe59a237b9394f2046f6e2d73e063287aef129e5
Flavio Lacks <flacks.cel@gmail.com>
2019-08-23 18:25:56 -0300

    DepartmentDao and SellerDao interfaces

---
+++ src/model/dao/ClienteDao.java
@@ -0,0 +1,15 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entity.Cliente;
+
+public interface ClienteDao {
+	
+	void insert (Cliente obj);
+	void update (Cliente obj);
+	void deleteById(Integer id);
+	Cliente findById(Integer id);
+	List<Cliente> findAll();
+
+}
